659915e87609574241d65fc518c06327f8016be7
codegenixdev <czhablog@gmail.com>
2025-01-02 12:31:42 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import PWABadge from "./PWABadge.tsx";
 
 function App() {
   const handleClick = () => {
-    alert("v1");
+    alert("v2");
   };
 
   return (
     <>
-      <button onClick={handleClick}>v1</button>
+      <button onClick={handleClick}>v2</button>
       <PWABadge />
     </>
   );
