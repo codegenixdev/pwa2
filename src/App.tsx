c86f57212d6d2faffde1d00a28199723cd1ca194
codegenixdev <czhablog@gmail.com>
2025-01-02 12:25:14 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import appLogo from "/favicon.svg";
-import PWABadge from "./PWABadge.tsx";
 import "./App.css";
+import PWABadge from "./PWABadge.tsx";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    alert("print");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="pwa2 logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        fooooooooooooooo
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        yoyosadji
-      </div>
+      <button onClick={handleClick}>print</button>
       <PWABadge />
     </>
   );
